55a2cef8a23a633aba2d26226f64243a6423bcab
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-13 16:42:25 -0400

    guess-flip test

--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,16 @@
 // Import the coinFlip function from your coin.mjs file
 import {coinFlip, flipACoin} from "./modules/coin.mjs";
+import minimist from 'minimist';
 // Call the coinFlip function and put the return into STDOUT
 console.log(flipACoin("heads"));
 console.log(flipACoin("tails"));
+
+
+// Call the coinFlip function and put the return into STDOUT
+
+const args = minimist(process.argv.slice(2))
+const str = args['call']
+if(!(str === "heads" || str === "tails" )){
+    throw new Error("User not found")
+}
+console.log(flipACoin(str));
