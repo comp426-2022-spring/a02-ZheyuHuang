454e0eac81674b059d1a0d9eff81dc6b155327cc
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-13 16:51:58 -0400

    guess flip update

--- guess-flip.js
+++ guess-flip.js
@@ -6,6 +6,9 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(2))
 const str = args['call']
 if(!(str === "heads" || str === "tails" )){
-    throw new Error("no input")
+    //throw new Error("no input")
+    console.log("Error: no input.");
+    console.log("Usage: node guess-flip --call=[heads|tails]");
+    process.exit();
 }
 console.log(flipACoin(str));
