b7504178bf6104b670fc4b9c9f75a42e7019fdd0
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-13 16:45:24 -0400

    guess-flip update

--- guess-flip.js
+++ guess-flip.js
@@ -2,11 +2,6 @@
 import {coinFlip, flipACoin} from "./modules/coin.mjs";
 import minimist from 'minimist';
 // Call the coinFlip function and put the return into STDOUT
-console.log(flipACoin("heads"));
-console.log(flipACoin("tails"));
-
-
-// Call the coinFlip function and put the return into STDOUT
 
 const args = minimist(process.argv.slice(2))
 const str = args['call']
